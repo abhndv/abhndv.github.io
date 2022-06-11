68a8fe0cbaefad8f1a58997e48e13ecae40a048b
abhndv <abhndv@gmail.com>
2022-06-11 14:41:25 +0000

    Deploying to gh-pages from  @ 65c349fce41a0652337b2b2c607e34e6b1971912 🚀

--- script.js
+++
@@ -1,7 +0,0 @@
-function showComingSoon() {
-  var x = document.getElementById("comingSoon");
-  x.className = "show";
-  setTimeout(function () {
-    x.className = x.className.replace("show", "");
-  }, 3000);
-}
